ff4559f64fb9f669fbbb8b747d975167bc932a0e
Bogdans <bogdans.cumaks@sk.lvt.lv>
2025-10-01 09:53:21 +0300

    izveidoju kaut ko

--- src/pakotne/GalvenaKlase.java
+++ src/pakotne/GalvenaKlase.java
@@ -1,1 +1,18 @@
-//nodzesu visu
+package pakotne;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class GalvenaKlase {
+    private int studSk, kritSk;
+    private String[] studenti;
+    private String[] kriteriji;
+    private int[] kriterijaSvars;
+    private int[][] kriterijaVertejums;
+    private double[] semestraVertejums;
+    private Scanner scan = new Scanner(System.in);
+    private DecimalFormat df = new DecimalFormat("0.#");
+
+    public static void main(String[] args) {
+    	
+    }
