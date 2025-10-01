c7be98e9e061e4658142acb9427d719ff4a2b3c2
Bogdans <bogdans.cumaks@sk.lvt.lv>
2025-10-01 09:54:28 +0300

    izveidoju kaut kadus atributus

--- src/pakotne/MetodesVert.java
+++ src/pakotne/MetodesVert.java
@@ -1,1 +1,13 @@
-//nodzesu visu
+package pakotne;
+
+    import java.text.DecimalFormat;
+    import java.util.Scanner;
+
+    public class MetodesVert {
+        private String[] studenti;
+        private String[] kriteriji;
+        private int[] kriterijaSvars;
+        private int[][] kriterijaVertejums;
+        private double[] semestraVertejums;
+        private Scanner scan;
+        private DecimalFormat df;
