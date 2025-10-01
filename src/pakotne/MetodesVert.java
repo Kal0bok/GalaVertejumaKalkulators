3ae2bf0605552c5b33b990ae1c373b81ec7f3d47
Bogdans <bogdans.cumaks@sk.lvt.lv>
2025-10-01 09:55:50 +0300

    Izveidoju metodes

--- src/pakotne/MetodesVert.java
+++ src/pakotne/MetodesVert.java
@@ -11,3 +11,43 @@ package pakotne;
         private double[] semestraVertejums;
         private Scanner scan;
         private DecimalFormat df;
+        
+        public MetodesVert(String[] studenti, String[] kriteriji, int[] kriterijaSvars,
+                int[][] kriterijaVertejums, double[] semestraVertejums,
+                Scanner scan, DecimalFormat df) {
+     this.studenti = studenti;
+     this.kriteriji = kriteriji;
+     this.kriterijaSvars = kriterijaSvars;
+     this.kriterijaVertejums = kriterijaVertejums;
+     this.semestraVertejums = semestraVertejums;
+     this.scan = scan;
+     this.df = df;
+ }
+
+ public void vertSk() {
+     for (int i = 0; i < kriterijaVertejums.length; i++) {
+         for (int j = 0; j < kriterijaVertejums[i].length; j++) {
+             do {
+                 System.out.println("Ievadi " + studenti[i] + " vērtējumu par kritēriju " + kriteriji[j]);
+                 while (!scan.hasNextInt()) {
+                     System.out.println("Ievadi " + studenti[i] + " vērtējumu par kritēriju " + kriteriji[j]);
+                     scan.next();
+                 }
+                 kriterijaVertejums[i][j] = scan.nextInt();
+             } while (kriterijaVertejums[i][j] < 0 || kriterijaVertejums[i][j] > 10);
+         }
+     }
+ }
+
+ public void vertApr() {
+     for (int i = 0; i < studenti.length; i++) {
+         double rezultats = 0;
+         for (int j = 0; j < kriteriji.length; j++) {
+             rezultats += ((double) kriterijaSvars[j] / 100) * kriterijaVertejums[i][j];
+         }
+         semestraVertejums[i] = rezultats;
+     }
+ }
+
+
+}
