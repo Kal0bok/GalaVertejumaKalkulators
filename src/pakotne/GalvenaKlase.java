c79f174ac8b4cba8b31bcbcc8d47248402c40db0
Bogdans <bogdans.cumaks@sk.lvt.lv>
2025-10-07 08:57:04 +0300

    Sapratu kapec kods nestrada, un salaboju

--- src/pakotne/GalvenaKlase.java
+++ src/pakotne/GalvenaKlase.java
@@ -103,36 +103,17 @@ public class GalvenaKlase {
                 }
             
 		
-		for(int i=0; i<kriterijaVertejums.length; i++) {
-			for(int j=0; j<kriterijaVertejums[i].length; j++) {
-				do {
-					System.out.println("Ievadi "+studenti[i]+" vērtējumu par kritēriju "+kriteriji[j]);
-					while(!scan.hasNextInt()) {
-						System.out.println("Ievadi "+studenti[i]+" vērtējumu par kritēriju "+kriteriji[j]);
-						scan.next();
-					}
-					kriterijaVertejums[i][j] = scan.nextInt();
-				}while(kriterijaVertejums[i][j]<0 || kriterijaVertejums[i][j]>10);
-			}
-		}
-		
-		double rezultats;
-		for(int i=0; i<studenti.length; i++) {
-			rezultats=0;
-			for(int j=0; j<kriteriji.length; j++) {
-				rezultats += ((double) kriterijaSvars[j]/100)*kriterijaVertejums[i][j];
-			}
-			semestraVertejums[i] = rezultats;
-		}
-		
-
-		for(int i=0; i<studenti.length; i++) {	
-			for(int j=0; j<kriteriji.length; j++) {
-				System.out.println("Studenta "+studenti[i]+" vērtējums par kritēriju "+kriteriji[j]+" ir "+kriterijaVertejums[i][j]+", kura svars ir "+kriterijaSvars[j]);
-			}
-			System.out.println("Semestra vērtējums ir "+df.format(semestraVertejums[i])+" balles"
-					+ "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
-		}
+                kriterijaSvars[i] = scan.nextInt();
+                atlSvars = (i < kriteriji.length - 1) ? (maxSvars - kriterijaSvars[i]) / (double) (kriteriji.length - i - 1) : 0;
+            } while (kriterijaSvars[i] > maxSvars || kriterijaSvars[i] < 5 ||
+                    (i != kriteriji.length - 1 && kriterijaSvars[i] == maxSvars) ||
+                    (i == kriteriji.length - 1 && maxSvars != kriterijaSvars[i]) ||
+                    (i < kriteriji.length - 1 && atlSvars < 5));
+            maxSvars -= kriterijaSvars[i];
+            scan.nextLine();
+        }
+        return kriterijaSvars;
+   
 		scan.close();
 	}
 }
